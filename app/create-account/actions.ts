4ed48135989b3a79b632061d1c10b00262289328
DESKTOP-70JUD3O\안하은 <jse033101@gmail.com>
2025-04-29 09:21:14 +0900

    주석 삭제

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
@@ -35,7 +36,6 @@ const formSchema = z
       .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirm_password: z.string().min(10),
   })
-  // form 전체에 대한 에러 {formErrors:[~~]} -> 에러의 책임 path 지정
   .refine(checkPasswords, {
     message: "Both passwords should be the same",
     path: ["confirm_password"],
@@ -48,14 +48,8 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  // parse는 유효성 검사가 실패하면 throw error -> try ~ catch 사용
-  // safeParse는 유효성 검사가 실패한다고해서 에러 throw하지 않음
   const result = formSchema.safeParse(data);
   if (!result.success) {
-    // result.error를 리턴하면 큰 object가 리턴되지만
-    // result.error.flatten()을 리턴하면 error가 굉장히 보기 편해짐
     return result.error.flatten();
   }
-
-  // 데이터 검증을 위해 if~~ 하고싶지 않다.
 }
