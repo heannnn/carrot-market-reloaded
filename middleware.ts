b2ac704cc5e68e89f4e4c07be1ddc89c5fc0e4fd
DESKTOP-70JUD3O\안하은 <jse033101@gmail.com>
2025-04-30 09:35:34 +0900

    로그인하지 않은 유저가 허락되지않은 url 접근시 메인페이지로 이동

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
 
-export async function middleware(request: NextRequest) {}
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  // 인증되지 않은 user가 갈 url
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/product", request.url));
+    }
+  }
+}
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
